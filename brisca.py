d079b178f87ce7514385c46defd88ebe41477864
Mar19845 <mar19845@uvg.edu.gt>
2022-09-29 17:39:06 -0600

    add brisca server

---
+++ brisca.py
@@ -0,0 +1,167 @@
+from random import randint
+import time
+
+
+class Player:
+    def __init__(self, socket, user_name):
+        self.socket = socket
+        self.user_name = user_name
+        self.state = 'chilling'  # inroom - playing
+
+    def change_state(self, state):
+        if state in ('chilling', 'inroom', 'playing'):
+            self.state = state
+
+    def write(self, content):
+        self.socket.write(content)
+
+    def get_user_name(self):
+        return self.user_name
+
+
+class Room:
+    def __init__(self, room_name):
+        self.room_name = room_name
+        self.table = None
+        self.turn = 0
+        self.round_picks = {}
+        self.total_picks = 0
+        self.scores = {}
+        self.players = []
+        self.player_list = []
+        self.deck = []
+        self.state = 'waiting'  # going
+        self.CARDS = ["clubs_1", "clubs_2", "clubs_3", "clubs_4", "clubs_5", "clubs_6", "clubs_7", "clubs_10", "clubs_11", "clubs_12",
+                      "hearts_1", "hearts_2", "hearts_3", "hearts_4", "hearts_5", "hearts_6", "hearts_7", "hearts_10", "hearts_11", "hearts_12",
+                      "spades_1", "spades_2", "spades_3", "spades_4", "spades_5", "spades_6", "spades_7", "spades_10", "spades_11", "spades_12",
+                      "diamonds_1", "diamonds_2", "diamonds_3", "diamonds_4", "diamonds_5", "diamonds_6", "diamonds_7", "diamonds_10", "diamonds_11", "diamonds_12"]
+        self.cards = self.CARDS.copy()
+
+    def change_state(self, state):
+        if state in ('waiting', 'going'):
+            self.state = state
+
+    def create_deck(self):
+        for i in range(0, 40):
+            u = randint(0, len(self.cards)-1)
+            self.deck.append(self.cards.pop(u))
+
+    def start_game(self):
+        print("room ready", self.room_name)
+        self.change_state('going')
+        self.turn = randint(0, 3)  # quien empieza
+        self.create_deck()  # crea la baraja
+        for player in self.players:
+            time.sleep(0.5)
+            content = {"status": "room_ready",
+                       "player": self.players[self.turn].get_user_name(), "cards": self.deck[:3], "triunf": self.deck[-1]}
+            self.player_list.append(player.get_user_name())
+            self.scores[player.get_user_name()] = 0
+            self.round_picks[player.get_user_name()] = ''
+            self.deck = self.deck[3:]
+            player.write(content)
+
+    def try_start(self):
+        if len(self.players) == 4:
+            self.start_game()
+
+    def check_picks(self):
+        triunf = self.CARDS.index(self.deck[-1])
+        palo = int(triunf / 10)
+        best = -1
+        user = ''
+        for player in self.round_picks.keys():
+            card = self.round_picks[player]
+            baza = self.CARDS.index(card)
+            valor = int(baza / 10)
+            if (palo == valor):
+                new_best = (baza % 10)+10
+                if (best < new_best):
+                    best = new_best
+                    user = player
+            else:
+                new_best = (baza % 10)
+                if (best < new_best):
+                    best = new_best
+                    user = player
+        return user
+
+    def calculate_scores(self):
+        for player in self.players:
+            player_name = player.get_user_name()
+            pick = self.round_picks[player_name]
+            baza = self.CARDS.index(pick)
+            result = baza % 10
+            if result == 0:
+                self.scores[player_name] += 11
+            if result == 2:
+                self.scores[player_name] += 10
+            if result == 7:
+                self.scores[player_name] += 2
+            if result == 8:
+                self.scores[player_name] += 3
+            if result == 9:
+                self.scores[player_name] += 4
+        self.round_picks = {}
+
+    def finish_round(self):
+        if len(self.deck) == 0:
+            content = {"status": "game_finished"}
+            for player in self.players:  # agregar todos los resultados
+                content[player.get_user_name(
+                )] = self.scores[player.get_user_name()]
+            for player in self.players:  # enviar todos los resultados completos
+                time.sleep(0.5)
+                player.write(content)
+
+        else:
+            winner = self.check_picks()
+            self.calculate_scores()
+            self.turn = self.player_list.index(winner)
+            for player in self.players:
+                time.sleep(0.5)
+                content = {"status": "round_finished",
+                           "winner": winner, "next_card": self.deck[:1]}
+                self.deck = self.deck[1:]
+                player.write(content)
+
+    def card_pick(self, card, player_name):
+        if self.state == 'going' and player_name == self.player_list[self.turn]:
+            self.round_picks[player_name] = card
+            self.total_picks += 1
+            self.turn = (self.turn + 1) % 4
+            for player in self.players:
+                content = {"status": "player_picked_card",
+                           "next_player": self.player_list[self.turn], "card": card, "picked": player_name}
+                player.write(content)
+            if self.total_picks == 4:
+                self.total_picks = 0
+                self.finish_round()
+                self.round_picks = {}
+        else:
+            print("ERROR IN CARD_PICK", self.state,
+                  player_name, self.player_list, self.turn)
+
+    def connect_player(self, new_player):
+        if new_player not in self.players and len(self.players) < 4:
+            for player in self.players:
+                print("sending entered to", player.get_user_name())
+                content = {"status": "player_entered", "room": self.room_name,
+                           "new_player": new_player.get_user_name()}
+                player.write(content)
+            self.players.append(new_player)
+            return True
+        return False
+
+    def disconnet_player(self, player):
+        self.players.remove(player)
+
+    def get_players_in_room(self):
+        players = []
+        for player in self.players:
+            players.append(player.get_user_name())
+        return players
+
+    def send_message_to_all(self, content):
+        for player in self.players:
+            player.write(content)
--- server.py
+++ server.py
@@ -1,73 +1,214 @@
-from concurrent.futures import process
-import socket
-import threading
 import sys
-import pickle
+import socket
+import selectors
+import traceback
+import lib
+import argparse
+from brisca import Room, Player
 
-class Server():
-    def __init__(self, host=socket.gethostname(), port=4000,num_connections=1):
-        self.initialize(host,port,num_connections)
+
+class Server:
+    def __init__(self, host='127.0.0.1', port=3000):
+        print("listening on", (host, port))
+        self.sel = selectors.DefaultSelector()
+        self.lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.lsock.bind((host, port))
+        self.lsock.listen()
+        self.lsock.setblocking(False)
+        self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
+        self.players = {}
+        self.rooms = {}
+
+    def accept_wrapper(self, sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        print("accepted connection from", addr)
+        conn.setblocking(False)
+        socket = lib.SocketHandler(self.sel, conn, addr)
+        self.sel.register(conn, selectors.EVENT_READ, data=socket)
+
+    def get_rooms(self):
+        rooms = []
+        for name in self.rooms.keys():
+            rooms.append(name)
+        return rooms
+
+    def get_players(self):
+        players = []
+        for name in self.players.keys():
+            players.append(name)
+        return players
+
+    def message_to_room(self, content, room):
+        room = self.rooms[room]
+        if room:
+            room.send_message_to_all(content)
+            return True
+        return False
+
+    def message_to_player(self, content, player):
+        player = self.players[player]
+        if player:
+            player.write(content)
+            return True
+        return False
+
+    def evaluate_request(self, socket):
+        request = socket.request
+        print(socket.addr, request)
+        close = False
+        write = True
+        action = request['action']
+        user = request['user']
+        if action == 'login':
+            print("agregar usuario")
+            player = Player(socket, user)
+            if user not in self.players.keys():
+                self.players[user] = player
+                print("usuario agregado")
+                content = {"status": "login", "rooms": self.get_rooms(
+                ), "players": self.get_players()}
+            else:
+                print("usuario ya existe")
+                content = {"status": "error",
+                           "message": f'User "{user}" exists'}
+                close = True
+
+        elif user in self.players.keys():
+            if action == 'get_rooms':
+                print("mostrar rooms")
+                content = {"status": "get_rooms", "rooms": self.get_rooms()}
+
+            elif action == 'create_room':
+                print("agregar room")
+                room_name = request['room']
+                room = Room(room_name)
+                if room_name not in self.rooms.keys():
+                    self.rooms[room_name] = room
+                    print("room agregado")
+                    content = {"status": "room_created",
+                               "rooms": self.get_rooms()}
+                else:
+                    print("room ya existe")
+                    content = {"status": "error",
+                               "message": f'Room "{room_name}" exists'}
+
+            elif action == 'join_room':
+                print("usuario unido a room")
+                room_name = request['room']
+                if room_name in self.rooms.keys():
+                    connected = self.rooms[room_name].connect_player(
+                        self.players[user])
+                    if connected:
+                        self.players[user].change_state('inroom')
+                        content = {"status": "join_room", "room": room_name,
+                                   "players_in_room": self.rooms[room_name].get_players_in_room()}
+                        socket.write(content)
+                        write = False
+                        self.rooms[room_name].try_start()
+                    else:
+                        content = {"status": "error",
+                                   "message": f'Room "{room_name}"is full'}
+
+                else:
+                    content = {"status": "error",
+                               "message": f'Room "{room_name}"doesnt exists'}
+
+            elif action == 'get_players':
+                print("mostrar jugadores")
+                content = {"status": "get_players",
+                           "players": self.get_players()}
+
+            elif action == 'message_to_room':
+                print("enviar mensaje a room")
+                room = request['room']
+                message = request['message']
+                content = {"status": "message_to_room",
+                           "from": user, "room": room, "message": message}
+                if(self.message_to_room(content, room)):
+                    content = {"status": "message_to_room_sent",
+                               "from": user, "room": room, "message": message}
+                else:
+                    content = {"status": "error",
+                               "message": 'couldnt send message to room'}
+
+            elif action == 'message_to_player':
+                print("enviar mensaje a jugador")
+                to = request['to']
+                message = request['message']
+                content = {"status": "message_to_player",
+                           "from": user, "to": to, "message": message}
+                if (self.message_to_player(content, to)):
+                    content = {"status": "message_to_player_sent",
+                               "from": user, "to": to, "message": message}
+                else:
+                    content = {"status": "error",
+                               "message": 'couldnt send message to player'}
+
+            elif action == 'card_pick':
+                print('seleccion del jugador')
+                room = request['room']
+                pick = request['card']
+                player_name = user
+                if room in self.rooms.keys():
+                    self.rooms[room].card_pick(pick, player_name)
+                    write = False
+                else:
+                    content = {"status": "error",
+                               "message": 'room doesnt exists'}
+
+            elif action == 'disconnect':
+                print("bye player")
+                self.players.pop(user)
+                content = {"status": "disconnect"}
+                close = True
+
+            else:
+                content = {"status": "error",
+                           "message": f'Command "{action}" doesnt exists'}
+        else:
+            content = {"status": "error", "message": 'login first'}
+
+        if write:
+            socket.write(content)
+        if close:
+            socket.close()
+
+    def start(self):
+        try:
+            while True:
+                events = self.sel.select(timeout=None)
+                for key, mask in events:
+                    if key.data is None:
+                        self.accept_wrapper(key.fileobj)
+                    else:
+                        socket = key.data
+                        try:
+                            if mask & selectors.EVENT_READ:
+                                socket.read()
+                            if mask & selectors.EVENT_WRITE:
+                                self.evaluate_request(socket)
+                        except Exception:
+                            print(
+                                "main: error: exception for",
+                                f"{socket.addr}:\n{traceback.format_exc()}",
+                            )
+                            socket.close()
+        except KeyboardInterrupt:
+            print("caught keyboard interrupt, exiting")
+        finally:
+            self.sel.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Brisca client.')
+    parser.add_argument('-p', dest='port', help='host to use')
+
+    args = parser.parse_args()
+
+    if args.port is None:
+        server = Server()
+        server.start()
+    else:
         
-        
-        # configure socket connection
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((str(self.host), int(self.port)))
-        self.sock.listen(self.num_connections)
-        self.sock.setblocking(False)
-        accept = threading.Thread(target=self.acceptConnection)
-        process = threading.Thread(target=self.proccessConnection)
-        
-        accept.daemon = True
-        accept.start()
-        process.daemon = True
-        process.start()
-
-        while True:
-            msg = input('->')
-            if msg == 'salir':
-                self.sock.close()
-                sys.exit()
-    def initialize(self,host,port,num_connections):
-        self.clients = []
-        self.host = host
-        self.port = port
-        self.num_connections = num_connections * 4
-
-        
-
-    def msg_to_all(self, msg, client):
-        for c in self.clients:
-            try:
-                if c != client:
-                    print(msg)
-                    c.send(msg)
-            except:
-                self.clients.remove(c)
-            
-            
-    def acceptConnection(self):
-		#print("aceptarCon iniciado")
-        while True:
-            try:
-                conn, addr = self.sock.accept()
-                conn.setblocking(False)
-                self.clients.append(conn)
-            except:
-                pass
-
-    def proccessConnection(self):
-        while True:
-            if len(self.clients) > 0:
-                for c in self.clients:
-                    try:
-                        data = c.recv(1024)
-                        if data:
-                            self.msg_to_all(data,c)
-                    except:
-                        pass'
-
-port = int(sys.argv[1])
-num_connections = int(sys.argv[2]) * 4 
-host = socket.gethostname()
-
-s = Server()
+        server = Server(host=args.port)
+        server.start()
